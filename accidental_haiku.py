af038efc0c23f6c237294918500c394ed87f7ad7
Emiel van Miltenburg <emielonline@gmail.com>
2020-04-13 16:11:38 +0200

    Update script, move general functions to utils.py

--- accidental_haiku.py
+++ accidental_haiku.py
@@ -1,32 +1,17 @@
-from string import punctuation
-PUNCTUATION = set(punctuation)
 import pyphen
-
 import spacy
-
-import ebooklib
-from ebooklib import epub
-from bs4 import BeautifulSoup
-
-def get_text(filename):
-    "Get text from an epub file."
-    book = epub.read_epub(filename)
-    texts = []
-    for doc in book.get_items_of_type(ebooklib.ITEM_DOCUMENT):
-        content = doc.content
-        soup = BeautifulSoup(content, features="lxml")
-        texts.append(soup.text)
-    return('\n'.join(texts))
+from utils import get_text, detokenize
 
 dic = pyphen.Pyphen(lang='nl_NL')
 
 def count_syllables(token):
     "Count the number of syllables in a token."
-    if token in PUNCTUATION:
+    if token.is_punct:
         return 0
-    hyphenated = dic.inserted(token)
+    hyphenated = dic.inserted(token.orth_)
     syllables = hyphenated.split('-')
     return len(syllables)
+
 
 def check_haiku(sentence):
     first, second, third = 0, 0, 0
@@ -34,7 +19,6 @@ def check_haiku(sentence):
     second_line = []
     third_line = []
     for token in sentence:
-        token = token.orth_
         syllables = count_syllables(token)
         if first < 5:
             first += syllables
@@ -53,14 +37,15 @@ def check_haiku(sentence):
                 third_line.append(token)
     if any([first != 5, second != 7, third != 5]):
         return None
-    first_line = ' '.join(first_line)
-    second_line = ' '.join(second_line)
-    third_line = ' '.join(third_line)
+    first_line = detokenize(first_line)
+    second_line = detokenize(second_line)
+    third_line = detokenize(third_line)
     return first_line, second_line, third_line
-        
+
+################################################################################
+# This does the actual work:
 
 nlp = spacy.load('nl_core_news_sm')
-
 text = get_text('viss012leve01_01.epub')
 doc = nlp(text)
 print("Start!")
