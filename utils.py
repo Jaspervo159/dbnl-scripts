35715fbb0138ff328e0d2ab9c371238f0e69c4d0
Emiel van Miltenburg <emielonline@gmail.com>
2020-04-13 17:11:30 +0200

    Refactor code.

--- utils.py
+++ utils.py
@@ -2,9 +2,11 @@ import ebooklib
 from ebooklib import epub
 from bs4 import BeautifulSoup
 import json
+import urllib.request
+import os
 
 ################################################################################
-# Load downloaded data from DBNL.
+# Load downloaded data from DBNL, select relevant entries, and download specific entries.
 
 def load_dbnl_data():
     "Load data from DBNL"
@@ -13,6 +15,46 @@ def load_dbnl_data():
         for entry in data:
             entry['genres'] = set(entry['genres'])
         return data
+
+
+def select_entries(data, 
+           genres, 
+           exact=False, 
+           year_start=0, 
+           year_end=3000,
+           need_epub=True,
+           need_pdf=True):
+    "Select books from DBNL."
+    results = []
+    genres = set(genres)
+    for entry in data:
+        if not genres.issubset(entry['genres']):
+            continue
+        if exact and not genres == entry['genres']:
+            continue
+        if need_epub and not 'epub' in entry['download']:
+            continue
+        if need_pdf and not 'pdf' in entry['download']:
+            continue
+        # Warning: only works for entries where exact year is specified!
+        if not entry['year'].isdigit():
+            continue
+        if year_start <= int(entry['year']) <= year_end:
+            results.append(entry)
+    return results
+
+
+def download(url, folder):
+    "Download file to a folder."
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+    filename = url.split('/')[-1]
+    urllib.request.urlretrieve(url, folder + filename)
+
+
+def download_entry(entry, folder):
+    "Download entry to a folder."
+    download(entry['download']['epub'], folder)
 
 ################################################################################
 # Load text from epub.
